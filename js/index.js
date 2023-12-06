9255e89056dbf0e0363f2ade04af2a6e9e0c8392
Rafaeldev22 <Rafael.dev.22@gmail.com>
2023-12-06 13:10:10 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let fade = document.querySelector("#modal-filme");
+// let modal = document.querySelector("#back");
+// let capa = document.querySelector(".capas");
+
+
+// capa.addEventListener('click', ()=>{
+
+//     toggleModal()
+// })
+
+// function toggleModal() {
+//     fade.classList.toggle("hide");
+//     modal.classList.toggle("hide");
+// }

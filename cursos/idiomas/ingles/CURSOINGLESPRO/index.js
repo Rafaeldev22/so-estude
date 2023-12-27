ba3446d9e1ecafed671ba0706d6e03193d990362
Rafaeldev22 <Rafael.dev.22@gmail.com>
2023-12-27 15:44:55 -0300

    atualizações de paginas

---
+++ cursos/idiomas/ingles/CURSOINGLESPRO/index.js
@@ -0,0 +1,43 @@
+const abrir = document.querySelector("#abrir-modal");
+const fechar = document.querySelector("#fechar");
+const fade = document.querySelector("#fade");
+const modal = document.querySelector("#card-modal");
+const iframe = document.querySelector(".game");
+const voltar = document.querySelector(".voltar");
+
+fechar.addEventListener("click", () => {
+  fecharVideo();
+  toggleModal();
+});
+
+function toggleModal() {
+  fade.classList.toggle("hide");
+  modal.classList.toggle("hide");
+  fecharVideo();
+}
+
+[abrir, fechar, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
+function fabrir() {
+  toggleModal();
+}
+
+function fecharVideo() {
+  let iframe = document.querySelector(".game");
+  iframe.src = "";
+}
+
+function desabilitarFull() {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    iframe.removeAttribute("allowfullscreen");
+  }
+}
+
+// Chamar a função de desabilitar fullscreen no carregamento da página
+desabilitarFull();
